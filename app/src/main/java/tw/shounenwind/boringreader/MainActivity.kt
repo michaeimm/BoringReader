9813861fc1da57b74b0ab6bbc51f48cbb2fcfb13
wind <k236michaeimm@gmail.com>
2020-01-17 02:21:01 +0800

    view model

--- app/src/main/java/tw/shounenwind/boringreader/MainActivity.kt
+++ app/src/main/java/tw/shounenwind/boringreader/MainActivity.kt
@@ -10,6 +10,7 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.core.content.edit
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.turingtechnologies.materialscrollbar.CustomIndicator
 import kotlinx.android.synthetic.main.activity_main.*
@@ -18,14 +19,9 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import okio.buffer
-import okio.source
 import org.jetbrains.anko.defaultSharedPreferences
 import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.toast
-import org.mozilla.universalchardet.UniversalDetector
-import java.io.InputStream
-import java.nio.charset.Charset
 
 
 class MainActivity : BaseActivity() {
@@ -34,6 +30,12 @@ class MainActivity : BaseActivity() {
     private lateinit var sharedPreferences: SharedPreferences
     private var currentFile: Uri? = null
     private var lineNumber = 0
+    private val viewModel = MainViewModel()
+    private var pd: ProgressDialog? = null
+        set(value) {
+            field?.dismiss()
+            field = value
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +46,10 @@ class MainActivity : BaseActivity() {
         setSupportActionBar(toolbar)
         list_view.layoutManager = LinearLayoutManager(this)
         list_view.adapter = adapter
+        viewModel.lines.observe(this, Observer {
+            bindData(it)
+        })
+
         dragScrollBar.apply {
             setIndicator(
                 CustomIndicator(this@MainActivity),
@@ -54,27 +60,23 @@ class MainActivity : BaseActivity() {
         val intent = intent
         val intentType = intent.type ?: ""
         if (savedInstanceState?.getString("currentFile") != null) {
-            val pd = ProgressDialog(this).show {
+            pd = ProgressDialog(this).show {
                 setContent(getString(R.string.loading))
                 setCancelable(false)
             }
             GlobalScope.launch {
-                val content = getFileContent(Uri.parse(savedInstanceState.getString("currentFile")))
-                bindData(content)
-                withContext(Dispatchers.Main) { pd.dismiss() }
+                getFileContent(Uri.parse(savedInstanceState.getString("currentFile")))
             }
         } else if (intent != null
             && (Intent.ACTION_SEND == intent.action || Intent.ACTION_VIEW == intent.action)
         ) {
             if (intentType.startsWith("text/")) {
-                val pd = ProgressDialog(this).show {
+                pd = ProgressDialog(this).show {
                     setContent(getString(R.string.loading))
                     setCancelable(false)
                 }
                 GlobalScope.launch {
-                    val content = getFileContent(intent.data!!)
-                    bindData(content)
-                    withContext(Dispatchers.Main) { pd.dismiss() }
+                    getFileContent(intent.data!!)
                 }
             } else {
                 toast(R.string.unsupported_format)
@@ -124,14 +126,12 @@ class MainActivity : BaseActivity() {
         super.onActivityResult(requestCode, resultCode, data)
         when {
             requestCode == 0 && resultCode == Activity.RESULT_OK && data?.data != null -> {
-                val pd = ProgressDialog(this).show {
+                pd = ProgressDialog(this).show {
                     setContent(getString(R.string.loading))
                     setCancelable(false)
                 }
                 GlobalScope.launch {
-                    val content = getFileContent(data.data!!)
-                    bindData(content)
-                    withContext(Dispatchers.Main) { pd.dismiss() }
+                    getFileContent(data.data!!)
                 }
             }
         }
@@ -147,82 +147,19 @@ class MainActivity : BaseActivity() {
         super.onPause()
     }
 
-    private suspend fun bindData(data: List<String>) = withContext(Dispatchers.Main) {
+    private fun bindData(data: List<String>) {
         Log.d("binding", "size: " + data.size)
         adapter.data = data
         hello_world.visibility = View.GONE
         list_view.scrollToPosition(lineNumber)
+        pd = null
     }
 
-    private suspend fun getFileContent(uri: Uri) = withContext<List<String>>(Dispatchers.IO) {
-        Log.d("open", uri.toString())
-        var inputStream: InputStream? = null
-        try {
-            currentFile = uri
-            lineNumber = sharedPreferences.getInt(uri.toString(), 0)
-            val cR = contentResolver
+    private suspend fun getFileContent(uri: Uri) = withContext(Dispatchers.IO) {
+        currentFile = uri
+        lineNumber = sharedPreferences.getInt(uri.toString(), 0)
 
-            inputStream = cR.openInputStream(uri)!!
-            val fileSource = inputStream.source()
-            val bufferedSource = fileSource.buffer()
-            val detector = UniversalDetector {
-
-            }
-
-            val data = bufferedSource.peek().readByteArray()
-            detector.handleData(data, 0, data.size)
-
-            val charset = if (detector.detectedCharset == null) {
-                withContext(Dispatchers.Main) {
-                    toast(R.string.unknown_charset)
-                }
-                Charset.defaultCharset()
-            } else {
-                charset(detector.detectedCharset)
-            }
-
-            Log.d("charset", charset.name())
-
-            val result = ArrayList<String>()
-            while (true) {
-                val lineBreakPosition = bufferedSource.peek().indexOf('\n'.toByte())
-                val str = when {
-                    lineBreakPosition < 0 -> {
-                        bufferedSource.readString(charset)
-                    }
-                    lineBreakPosition == 0L -> {
-                        bufferedSource.skip(1)
-                        " "
-                    }
-                    else -> {
-                        bufferedSource.readString(lineBreakPosition, charset).apply {
-                            substring(0, length)
-                            bufferedSource.skip(1)
-                        }
-                    }
-                }
-
-                if (str.isEmpty() && bufferedSource.peek().indexOf('\n'.toByte()) == -1L) {
-                    break
-                }
-
-                result.add(str)
-
-                if (isFinishing) {
-                    break
-                }
-            }
-            return@withContext result
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-            withContext(Dispatchers.Main) {
-                toast(R.string.error_and_close)
-            }
-            return@withContext emptyList()
-        } finally {
-            inputStream?.close()
-        }
+        viewModel.getFileContent(this@MainActivity, uri)
     }
 
 }
---
+++ app/src/main/java/tw/shounenwind/boringreader/MainViewModel.kt
@@ -0,0 +1,92 @@
+package tw.shounenwind.boringreader
+
+import android.content.Context
+import android.net.Uri
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.withContext
+import okio.buffer
+import okio.source
+import org.jetbrains.anko.toast
+import org.mozilla.universalchardet.UniversalDetector
+import java.io.InputStream
+import java.nio.charset.Charset
+
+class MainViewModel : ViewModel() {
+
+    private val _lines = MutableLiveData<List<String>>()
+    val lines: LiveData<List<String>> = _lines
+
+    suspend fun getFileContent(mContext: Context, uri: Uri) = withContext(Dispatchers.IO) {
+        Log.d("open", uri.toString())
+        var inputStream: InputStream? = null
+        try {
+
+            val cR = mContext.contentResolver
+
+            inputStream = cR.openInputStream(uri)!!
+            val fileSource = inputStream.source()
+            val bufferedSource = fileSource.buffer()
+            val detector = UniversalDetector {
+
+            }
+
+            val data = bufferedSource.peek().readByteArray()
+            detector.handleData(data, 0, data.size)
+
+            val charset = if (detector.detectedCharset == null) {
+                withContext(Dispatchers.Main) {
+                    mContext.toast(R.string.unknown_charset)
+                }
+                Charset.defaultCharset()
+            } else {
+                charset(detector.detectedCharset)
+            }
+
+            Log.d("charset", charset.name())
+
+            val result = ArrayList<String>()
+            while (true) {
+                val lineBreakPosition = bufferedSource.peek().indexOf('\n'.toByte())
+                val str = when {
+                    lineBreakPosition < 0 -> {
+                        bufferedSource.readString(charset)
+                    }
+                    lineBreakPosition == 0L -> {
+                        bufferedSource.skip(1)
+                        " "
+                    }
+                    else -> {
+                        bufferedSource.readString(lineBreakPosition, charset).apply {
+                            substring(0, length)
+                            bufferedSource.skip(1)
+                        }
+                    }
+                }
+
+                if (str.isEmpty() && bufferedSource.peek().indexOf('\n'.toByte()) == -1L) {
+                    break
+                }
+
+                result.add(str)
+
+                if (!isActive) {
+                    break
+                }
+            }
+            _lines.postValue(result)
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            withContext(Dispatchers.Main) {
+                mContext.toast(R.string.error_and_close)
+            }
+        } finally {
+            inputStream?.close()
+        }
+    }
+}
